010359d1ed8b456cfc97f7407617178c9b1392cf
mtabaie <mtabaie@gmail.com>
2023-08-16 19:20:14 +0330

    upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+    fontFamily:{
+      "comic":["Comic Sans MS","sans serif"],
+      "IranSans":["IranSans"]
+    }
+  },
+  plugins: [],
+}
